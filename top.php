65fb162b4ba716999515ce58c717ea4d1c5bc042
Pedrojuaba <phgcoelho@outlook.com>
2023-06-21 22:02:41 -0300

    separação em blocos (top e bottom)

---
+++ top.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <title>Ubiq Bot - Seu parceiro para uma comunidade mais segura</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <header class="container">
+            <nav class="nav">
+                <section class="logo">
+                    <img src="./images/logo.svg" alt="Logo Robo Ubiq" />
+                    <p class="logo-name">Ubiq</p>
+                </section>
+                <section class="nav-links">
+                    <ul class="links">
+                        <li><a class="link atention-color" href="index.php">Inicio</a></li>
+                        <li><a class="link" href="sobre.php">Sobre</a></li>
+                        <li><a class="link" href="funcionalidades.php">Funcionalidades</a></li>
+                        <li><a class="link" href="valores.php">Valores</a></li>
+                    </ul>
+                    <a class="signup" href="home.php" target="_blank">Iniciar</a>
+                </section>
+            </nav>
+        </header>
+        <section>
