9a51c482e09bd160ce5f2774b4fa3d0cbbb8d3cc
Bernard2307 <bernardlnogueira@outlook.com>
2023-06-21 21:20:37 -0300

    titulo cadaastro

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="cadastro.css" rel="stylesheet">
-  <title>Cadastro</title>
+  <title>LOGIN |UBIQ</title>
 </head>
 <body>
   <a href="home.php">Voltar</a>
