728d838032ebc3ade5813e9715fc1b04adf13a0f
ardotheedu <eduardosalima3@gmail.com>
2023-06-26 22:38:05 -0300

    feat: adicona o upload de image

--- painel.php
+++ painel.php
@@ -1,5 +1,12 @@
 <?php
    include("protect.php");
+   
+   if (isset($_SESSION["profilePicPath"])) {
+       $profilePicPath = $_SESSION["profilePicPath"];
+   } else {
+       // Set a default profile picture path if no picture has been uploaded yet
+       $profilePicPath = "profile_pics/default.png";
+   }
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +22,13 @@
     <br><br>
 
     <h1>PAINEL</h1>
+    <img src="<?php echo $profilePicPath; ?>" alt="Profile Picture">
     <br>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <label for="profilePic">Profile Picture:</label>
+        <input type="file" name="profilePic" id="profilePic">
+        <input type="submit" value="Upload">
+    </form>
     <p>Imagem do usuario ficaria aqui</p>
     <p>Nome: <?php echo $_SESSION['nome']; ?></p>
     <input class="submit" type="submit" name="alterar" value="Alterar informações do usuário">
